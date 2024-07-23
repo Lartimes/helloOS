19c6695695ff9bee0c5bd50ab48637794bbe79cc
Lartimes <lartimes2004@163.com>
2024-07-24 01:30:55 +0800

    day03-pointer

--- assm/bootpack.c
+++ assm/bootpack.c
@@ -3,8 +3,16 @@
 //
 void io_hlt(void);
 
+
 void HariMain(void) {
-fin:
-    io_hlt(); /*执行naskfunc.nas里的_io_hlt*/
-    goto fin;
+    int i; /*变量声明：i是一个32位整数*/
+    for (i = 0xa0000; i <= 0xaffff; i++) {
+//        p = (char *) i ;
+//        *p = i & 0x0f;
+        *((char *) i) = i & 0x0f;
+    }
+
+    for (;;) {
+        io_hlt();
+    }
 }
