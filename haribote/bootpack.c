5e0fdab8b8fcca4aa3332202f6dac5c1ff2d39c8
Lartimes <lartimes2004@163.com>
2024-08-09 00:07:06 +0800

    day09-memory_test

--- haribote/bootpack.c
+++ haribote/bootpack.c
@@ -4,6 +4,12 @@
 
 extern struct FIFO8 keyfifo, mousefifo;
 
+#define EFLAGS_AC_BIT 0x00040000
+#define CR0_CACHE_DISABLE 0x60000000
+void store_cr0(int cr0);
+int load_cr0(void);
+unsigned int memtest_sub(unsigned int start, unsigned int end);
+unsigned int memtest(unsigned int start, unsigned int end);
 
 /**
  * 主程序MAIN 入口
@@ -39,6 +45,11 @@ void HariMain(void) {
     mx = (binfo->scrnx - 16) / 2; /* 计算画面的中心坐标*/
     my = (binfo->scrny - 28 - 16) / 2;
     putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+//    ------------------------------------------------------------------
+    i = memtest(0x00400000, 0xbfffffff) / (1024 * 1024);
+    sprintf(s, "memory %dMB", i);
+    putfonts8_asc(binfo->vram, binfo->scrnx, 100, 32, COL8_FFFFFF, s);
+
 //    ------------------------------------------------------------------
     enable_mouse(&mdec);
     for (;;) {
@@ -94,6 +105,37 @@ void HariMain(void) {
         }
     }
 }
+
+
+
+unsigned int memtest(unsigned int start, unsigned int end)
+{
+    char flg486 = 0;
+    unsigned int eflg, cr0, i;
+/* 确认CPU是386还是486以上的 */
+    eflg = io_load_eflags();
+    eflg |= EFLAGS_AC_BIT; /* AC-bit = 1 */
+    io_store_eflags(eflg);
+    eflg = io_load_eflags();
+    if ((eflg & EFLAGS_AC_BIT) != 0) { /* 如果是386，即使设定AC=1，AC的值还会自动回到0 */
+        flg486 = 1;
+    }
+    eflg &= ~EFLAGS_AC_BIT; /* AC-bit = 0 */
+    io_store_eflags(eflg);
+    if (flg486 != 0) {
+        cr0 = load_cr0();
+        cr0 |= CR0_CACHE_DISABLE; /* 禁止缓存 */
+        store_cr0(cr0);
+    }
+    i = memtest_sub(start, end);
+    if (flg486 != 0) {
+        cr0 = load_cr0();
+        cr0 &= ~CR0_CACHE_DISABLE; /* 允许缓存 */
+        store_cr0(cr0);
+    }
+    return i;
+}
+
 
 
 
